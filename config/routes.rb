be57e646121fcb8ae3156b9e52057f5532ade7ff
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-09-06 19:29:19 +0300

    Fix another typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   patch 'team.:id' => 'teams#update'
   resources :pages,               only: [:new, :create, :edit, :update,
                                          :destroy]
-  get 'invites/send-invite'   => 'teams#invite'
+  get 'invites/send-invite'   => 'teams#send_invite'
   get 'invites/cancel-invite' => 'teams#cancel_invite'
   get 'invites/accept-invite' => 'teams#accept_invite'
   get '/:id'       => 'pages#show'
