c7a5291a60b3ea7a6efc82ad87f727fd71ebc9d2
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-09-29 15:24:52 +0300

    Add participant counter for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+    @participant_count = User.all.count - User.where(admin: true).size
   end
 
   def check
