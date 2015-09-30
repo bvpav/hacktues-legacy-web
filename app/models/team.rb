db9c290c7ad7678dd19ff44f16cd02cbd2dd9ab5
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-09-30 21:17:11 +0300

    Remove technologies presence validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,6 @@ class Team < ActiveRecord::Base
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :project_desc,  presence: true, length: { minimum: 50, maximum: 500 }
-  validates :technologies, presence: true, length: { maximum: 5 }
+  validates :technologies, length: { maximum: 5 }
   validates :members_id, length: { maximum: 4 }
 end
