72df7c507d0b56438549d801f18853ce40b2275d
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-10-02 18:34:53 +0300

    Precompile assets to apply youtube embed css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,34 +9,34 @@ class ApplicationController < ActionController::Base
   after_filter :cors_set_access_control_headers
   skip_before_filter :verify_authenticity_token
 
-# For all responses in this controller, return the CORS access control headers.
+  # For all responses in this controller, return the CORS access control headers.
 
-def cors_set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  headers['Access-Control-Request-Method'] = '*'
-  headers['Access-Control-Allow-Headers'] = '*'
-  headers['Access-Control-Max-Age'] = "1728000"
-end
-
-# If this is a preflight OPTIONS request, then short-circuit the
-# request, return only the necessary headers and return an empty
-# text/plain.
-
-def cors_preflight_check
-  if request.method == :options
+  def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = '*'
-    render :text => '', :content_type => 'text/plain'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
-end
-  include SessionsHelper
 
-  private
-  def expire_hsts
-    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Max-Age'] = '*'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
-end
+    include SessionsHelper
+
+    private
+    def expire_hsts
+      response.headers["Strict-Transport-Security"] = 'max-age=0'
+    end
+  end
