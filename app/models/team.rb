44f4d04eb14b1f8c9a063aa4c72d06cb69dd74f4
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-10-03 20:44:25 +0300

    izi pizi limoni

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,7 @@ class Team < ActiveRecord::Base
   serialize :technologies, Array
 
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :project_desc,  presence: true, length: { minimum: 50, maximum: 500 }
+  # validates :project_desc,  presence: true, length: { minimum: 1, maximum: 500 }
   validates :technologies, length: { maximum: 10 }
-  validates :members_id, length: { maximum: 4 }
+  # validates :members_id, length: { maximum: 4 }
 end
